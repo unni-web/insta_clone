95a7820668555a989e6162397b6b8194bd65d263
unni-web <unnikrish1011@gmail.com>
2025-10-28 16:07:42 +0530

    home page completed

--- insta/app/views.py
+++ insta/app/views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+
 
 # Create your views here.
 def register(request):
+    if request.method=='POST':
+        email=request.POST['email']
+        password=request.POST['password']
+        name=request.POST['name']
+        username=request.POST['username']
+        data=User.objects.create_user(email=email,password=password,first_name=name,username=username)
+        data.save()
+        return redirect(loginuser)
     return render(request,'register.html')
 
 def loginuser(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect(index)
+        else:
+            return redirect(loginuser)
     return render(request,'login.html')
 
 def index(request):
-    return render(request,'index.html')
+    if request.user.is_authenticated:
+
+        return render(request,'index.html')
+    else:
+        return redirect(loginuser)
